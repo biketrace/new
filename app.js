9e6d836de2d0f7f7e4583da7f706339d4f8e137b
Gemmy2010 <42281543+Gemmy2010@users.noreply.github.com>
2021-09-18 13:27:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
+const navLink = document.querySelectorAll(".nav-link");
 
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
 
 /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
 var acc = document.getElementsByClassName("accordion");
@@ -18,68 +25,3 @@ for (i = 0; i < acc.length; i++) {
     this.nextElementSibling.classList.toggle("show");
   };
 }
-
-
-/*
-
-
-
-
-
-
-
-
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-window.addEventListener("DOMContentLoaded", function () {
-  // get the form elements defined in your form HTML above
-
-  var form = document.getElementById("my-form");
-  // var button = document.getElementById("my-form-button");
-  var status = document.getElementById("status");
-
-  // Success and Error functions for after the form is submitted
-
-  function success() {
-    form.reset();
-    status.classList.add("success");
-    status.innerHTML = "Thanks,you are one of us now!";
-  }
-
-  function error() {
-    status.classList.add("error");
-    status.innerHTML = "Oops! There was a problem.";
-  }
-
-  // handle the form submission event
-
-  form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data, success, error);
-  });
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}*/
